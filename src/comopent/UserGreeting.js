028d5224caa8b4c4ce98ded79ab01ef3797ed919
vaishalimasal <98303984+vaishalimasal@users.noreply.github.com>
2022-08-25 12:27:29 +0530

    Short Circuit Operator approach to use react conditional rendering

--- src/comopent/UserGreeting.js
+++ src/comopent/UserGreeting.js
@@ -5,17 +5,29 @@ class UserGreeting extends Component {
     super(props);
 
     this.state = {
-      isLoggedIn: false,
+      isLoggedIn: true,
     };
   }
   render() {
-    let message; //Element Variables approach
-    if (this.state.isLoggedIn) {
-      message = <div>Welcome Vaishali</div>;
-    } else {
-      message = <div>Welcome Guest</div>;
-    }
-    return <div>{message} </div>;
+    //Short Circuit Operator
+    return this.state.isLoggedIn && <div>Welcome Vaishali</div>;
+
+    // //Ternary Conditional Operator
+    // return this.state.isLoggedIn ? (
+    //   <div>Welcome Vaishali</div>
+    // ) : (
+    //   <div>Welcome Guest</div>
+    // );
+
+    // //Element Variables approach
+    // let message;
+    // if (this.state.isLoggedIn) {
+    //   message = <div>Welcome Vaishali</div>;
+    // } else {
+    //   message = <div>Welcome Guest</div>;
+    // }
+    // return <div>{message} </div>;
+    //if/esle approach
     // return (
     //     <div>
     //         <div>Welcome Vaishali</div>
