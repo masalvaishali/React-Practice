ee70d62ecdd22fe7e34d89c9e2271d922bfc8e51
vaishalimasal <98303984+vaishalimasal@users.noreply.github.com>
2022-08-25 12:11:03 +0530

    Element Variables approach for if/else

--- src/comopent/UserGreeting.js
+++ src/comopent/UserGreeting.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class UserGreeting extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            isLoggedIn: false
-        }
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  render() {
+    let message; //Element Variables approach
+    if (this.state.isLoggedIn) {
+      message = <div>Welcome Vaishali</div>;
+    } else {
+      message = <div>Welcome Guest</div>;
     }
-    render() {
-        if(this.state.isLoggedIn){
-            return   <div>Welcome Vaishali</div>
-        }else 
-        {
-            return      <div>Welcome Guest</div>
-        }
-        // return (
-        //     <div>
-        //         <div>Welcome Vaishali</div>
-        //         <div>Welcome Guest</div>
-        //     </div>
-        // )
-    }
+    return <div>{message} </div>;
+    // return (
+    //     <div>
+    //         <div>Welcome Vaishali</div>
+    //         <div>Welcome Guest</div>
+    //     </div>
+    // )
+  }
 }
 
-export default UserGreeting
+export default UserGreeting;
